3721ee048de6862f5d9be8001126e9b2b29c16bb
season101 <sijan.malla101@gmail.com>
2019-10-15 21:47:24 -0500

    To Run for first time

---
+++ main.py
@@ -0,0 +1,44 @@
+
+# ###################### Introduction #############################
+#                                                                 #
+#    #Name: Sijan Malla              Date Assigned:Sept. 24,2019  #
+#    #                                                            #
+#    #Course: 2000-44306             Date Due : Oct. 1, 2019      #
+#    #                                                            #
+#    #File name: Programming Assignment 2                         #
+#    #                                                            #
+#    #Program Description: It acts as a webiste for a amusement   #
+#                          park for selling the tickets.          #
+#                                                                 #
+##################### Program Begins ##############################
+
+#Title of Program
+print()
+print("{0:^60s}".format("=-"*20))
+print()
+print("{0:^60s}".format("DECODE MESSAGES HERE!"))
+print()
+print("{0:^60s}".format("=-"*20))
+print()
+print("="*60)
+print()
+
+#Taking fileName from the user
+fileName = input("Enter encoded file name: ")
+print()
+print()
+
+#To Display Title "DECODED MESSAGE"
+style = "+"+"-"*17+"+"
+print("{0:^58s}".format(style))
+print(" "*2+"-"*17+"|"+" DECODED MESSAGE "+"|"+"-"*19)
+print("{0:^58s}".format(style))
+print()
+
+print()
+
+#Opening up the file to DECODE
+fileToDecode= open("encoded_2.txt","r")
+for each in fileToDecode:
+    print(each.rsplit())
+fileToDecode.close()
