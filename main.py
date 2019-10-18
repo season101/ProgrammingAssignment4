2b1a21dce369ae252536be74e9db7666190d2e59
season101 <sijan.malla101@gmail.com>
2019-10-17 23:41:59 -0500

    Fully finished

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 # ###################### Introduction #############################
 #                                                                 #
-#    #Name: Sijan Malla              Date Assigned:Sept. 24,2019  #
+#    #Name: Sijan Malla              Date Assigned: Oct. 10,2019  #
 #    #                                                            #
-#    #Course: 2000-44306             Date Due : Oct. 1, 2019      #
+#    #Course: 2000-44306             Date Due : Oct. 24, 2019     #
 #    #                                                            #
 #    #File name: Programming Assignment 4                         #
 #    #                                                            #
-#    #Program Description: It acts as a webiste for a amusement   #
-#                          park for selling the tickets.          #
+#    #Program Description: It is a program that simply Decodes    #
+#                          the encoded text file                  #
 #                                                                 #
 ##################### Program Begins ##############################
 
@@ -33,7 +33,7 @@ while userChoice == "y":        #Looping for if user wants to run the program ag
 
     fileName = input("Enter encoded file name: ")       #asking user for the name of encoded file.
 
-    while not(".txt" in fileName):      #input validation for ensuring that the given file must be a .txt format
+    while fileName[len(fileName)-4: ]!=".txt":      #input validation for ensuring that the given file must be a .txt format
         fileName = input("Error! Enter encoded file name (only .txt format is accepted): ")
 
     print()
@@ -50,47 +50,59 @@ while userChoice == "y":        #Looping for if user wants to run the program ag
 
 
     #Opening up the file to DECODE using File Handling in Python
+
+
     fileToDecode= open(fileName,"r")        #Given encodde file is opened in read mode and stored as fileToDecode
 
     forInnerLoop = ""       #Declaring variable to be used inside the for everySentence loop.
     decodedSentence=""             #Declaring variable to be used inside the for everyCharacter Loop.
 
     for everySentence in fileToDecode:      #For loop to run until there is sentence in a given file. It reads each lines in single loop.
-        LineInFile = each.rstrip()      #LineInFile is used to store each lines one at a time so as to pass its value to inner for everyCharacter Loop.
+        LineInFile = everySentence     #LineInFile is used to store each lines one at a time so as to pass its value to inner for everyCharacter Loop.
 
         for everyCharacter in LineInFile:       #For loop to run until there is character in a given line. It reads each charcter in single loop.
 
-            if not(everyCharacter==" "):     #To check if given character is space i.e. word seperator. Since, we don't want to use any other character as word seperator. If true ie. given character is not " " it executes.
+            if ord(everyCharacter)>=33:     #To check if given character is space i.e. word seperator. Since, we don't want to use any other character as word seperator. If true ie. given character is not " " it executes.
 
                 encodedCharToASCII = ord(everyCharacter)     #ASCII value of encoded character
 
                 ASCIIForDecodedCharacter = encodedCharToASCII + 2       #Since message has been encoded by subtracting characters ascii value by 2. So we do reverse that is add 2 to decode the encoded message.
 
-                entence+= chr(ASCIIForDecodedCharacter)        #Accumulating character one after another in its decoded form
+                decodedSentence+= chr(ASCIIForDecodedCharacter)        #Accumulating character one after another in its decoded form
+
+            elif ord(everyCharacter)==31:
+                decodedSentence+="!"       #It is to ignore the fact that we want exclamation sign to be there.
 
             else:
                 decodedSentence+=" "       #It is to ignore the fact that we want space to be the word seperator but not any other charater. so, if space character is detected we leave it as it is.
 
         print(decodedSentence)     #To print the single linee of the decoded message from the encoded message
 
-        decodedSentencesentence=""     #Resetting the value for sentence to zero so that when new line is read from outer loop the values for earlier sentence in deleted from the variable "sentence"
+        decodedSentence=""     #Resetting the value for sentence to zero so that when new line is read from outer loop the values for earlier sentence in deleted from the variable "sentence"
 
     fileToDecode.close()        #To stop reading from input encoded text file
 
     print()
 
+
+    #Formatting for display of  "DECODED MESSAGE"
     style = "+"+"-"*17+"+"
+
     print("{0:^58s}".format(style))
     print(" "*2+"-"*17+"|"+"  END OF MESSAGE "+"|"+"-"*19)
     print("{0:^58s}".format(style))
-    print()
 
     print()
     print()
-    userChoice = input("Would you like to decode another file(y/n)?: ")
+
+    userChoice = input("Would you like to decode another file(y/n)?: ")     ##asking user if they want to run program again.
     userChoice = userChoice.lower()
-    while not(userChoice == "y" or userChoice == "n"):
+
+    while not(userChoice == "y" or userChoice == "n"):      #input validation for userchoice
         userChoice = input("Error! Enter your choice(y and n are only allowed): ")
 
 print()
-print("Thank you for using our program. Please come again!")
+
+print("Thank you for using our program. Please come again!") #Ending Prompt
+
+#Program ends here
